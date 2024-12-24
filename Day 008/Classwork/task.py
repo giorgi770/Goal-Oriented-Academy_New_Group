cacb4f6c7f3bd8ad646316420bbfbb9f8ed7df97
giorgi770 <varazagio370@gmail.com>
2024-12-24 10:12:49 -0800

    first commit

---
+++ Day 008/Classwork/task.py
@@ -0,0 +1,18 @@
+# 1) comparators operatorors 2-2 მაგალითი
+# #  >
+# #  <
+# #  >=
+# #  <=
+# #  ==
+# #  !=
+
+# # ✅
+# print(1 > 2)
+# print(1 < 2)
+
+print(7 > 2)
+print(8 < 3)
+print(5 >= 4)
+print(8 <= 9)
+print(15 == 10)
+print(3 != 2)
